77373929ebb6438567d54bfc7f07fe9998cc7645
pagamatteo <pagamatteo@gmail.com>
2021-05-28 23:25:03 +0200

    Add core attention classes.

---
+++ attention/analyzers.py
@@ -0,0 +1,217 @@
+import copy
+import pandas as pd
+from tqdm import tqdm
+from utils.result_collector import TestResultCollector
+
+
+class AttentionMapAnalyzer(object):
+    """
+    This class applies the analyzes implemented by appropriate tester classes to
+    the attention maps extracted by an appropriate attention extractor class.
+    It automatically categorizes the results of such analyzes into the following
+    categories:
+    - all: all the records of the dataset
+    - true_match: ground truth match records
+    - true_non_match: ground truth non-match records
+    - pred_match: match records predicted by the model
+    - pred_non_match: non-match records predicted by the model
+    It accepts multiple classes as input for the application of different tests.
+    Such classes have to implement the following interface:
+    - test(left_entity, right_entity, attn_params): applies some tests on the
+        attention maps (integrated with additional params) and returns the results
+    """
+
+    def __init__(self, attn_extractor, testers: list):
+
+        assert isinstance(testers, list), "Wrong data type for parameter 'testers'."
+        assert len(testers) > 0, "Empty tester list."
+
+        self.attn_extractor = attn_extractor
+        self.testers = testers
+        res = {
+            'all': None,
+            'true_match': None,
+            'true_non_match': None,
+            'pred_match': None,
+            'pred_non_match': None,
+            'tp': None,
+            'tn': None,
+            'fp': None,
+            'fn': None
+        }
+
+        self.res_history = [copy.deepcopy(res) for _ in range(len(self.testers))]
+        self.counts = {
+            'all': 0,
+            'true_match': 0,
+            'true_non_match': 0,
+            'pred_match': 0,
+            'pred_non_match': 0,
+            'tp': 0,
+            'tn': 0,
+            'fp': 0,
+            'fn': 0
+        }
+        self.preds = []
+        self.labels = []
+        self.text_units = []
+
+    def __len__(self):
+        return len(self.attn_extractor)
+
+    def _save_result(self, res_history: dict, result: (dict, TestResultCollector),
+                     result_name: str):
+
+        assert isinstance(res_history, dict), "Wrong data type for parameter 'res_history'."
+        assert isinstance(result, (dict, TestResultCollector)), "Wrong data type for parameter 'result'."
+        assert isinstance(result_name, str), "Wrong data type for parameter 'result_name'."
+        assert result_name in res_history
+
+        if res_history[result_name] is None:  # init the result collector
+
+            res_history[result_name] = copy.deepcopy(result)
+
+        else:  # cumulative sum of the results
+
+            if isinstance(result, dict):
+
+                for key in result:
+                    assert key in res_history[result_name]
+                    assert isinstance(res_history[result_name][key], TestResultCollector)
+                    assert isinstance(result[key], TestResultCollector)
+
+                    res_history[result_name][key].add_collector(result[key])
+
+            elif isinstance(result, TestResultCollector):
+
+                assert isinstance(res_history[result_name], TestResultCollector)
+
+                res_history[result_name].add_collector(result)
+
+        self.counts[result_name] += 1
+
+    def _save(self, results: list, label: int, pred: int, category=None):
+
+        assert isinstance(results, list), "Wrong data type for parameter 'results'."
+        assert len(results) == len(self.testers), "Length mismatch between 'results' and 'self.testers'."
+        assert isinstance(label, int), "Wrong data type for parameter 'label'."
+        assert isinstance(pred, int), "Wrong data type for parameter 'pred'."
+
+        for tester_idx in range(len(self.testers)):
+            tester_res_history = self.res_history[tester_idx]
+            tester_res = results[tester_idx]
+
+            self._save_result(tester_res_history, tester_res, 'all')
+
+            if label == 1:  # match row
+
+                self._save_result(tester_res_history, tester_res, 'true_match')
+
+                if pred == 1:   # true positive
+                    self._save_result(tester_res_history, tester_res, 'pred_match')
+                    self._save_result(tester_res_history, tester_res, 'tp')
+                else:           # false negative
+                    self._save_result(tester_res_history, tester_res, 'pred_non_match')
+                    self._save_result(tester_res_history, tester_res, 'fn')
+
+            else:  # non-match row
+
+                self._save_result(tester_res_history, tester_res, 'true_non_match')
+
+                if pred == 1:   # false positive
+                    self._save_result(tester_res_history, tester_res, 'pred_match')
+                    self._save_result(tester_res_history, tester_res, 'fp')
+                else:           # true negative
+                    self._save_result(tester_res_history, tester_res, 'pred_non_match')
+                    self._save_result(tester_res_history, tester_res, 'tn')
+
+            if category is not None:
+                if category not in tester_res_history:
+                    tester_res_history[category] = copy.deepcopy(tester_res)
+                else:
+                    self._save_result(tester_res_history, tester_res, category)
+
+    def __getitem__(self, idx: int):
+
+        left_entity, right_entity, attn_params = self.attn_extractor[idx]
+
+        assert isinstance(left_entity, pd.Series), "Wrong data type for parameter 'left_entity'."
+        assert isinstance(right_entity, pd.Series), "Wrong data type for parameter 'right_entity'."
+        assert isinstance(attn_params, dict), "Wrong data type for parameter 'attn_params'."
+        assert 'preds' in attn_params, "predictions not found."
+        assert 'labels' in attn_params, "labels not found."
+        assert 'text_units' in attn_params, "'text_units' not found."
+
+        label = attn_params['labels'].item()
+        pred = attn_params['preds'].item()
+        text_units = attn_params['text_units']
+        category = None
+        if 'category' in attn_params:
+            category = attn_params['category']
+
+        tester_results = []
+        for tester_id, tester in enumerate(self.testers):
+            result = tester.test(left_entity, right_entity, attn_params)
+
+            if result is not None:
+                assert isinstance(result, (dict, TestResultCollector))
+                if isinstance(result, dict):
+                    for key in result:
+                        assert isinstance(result[key], TestResultCollector)
+
+            tester_results.append(result)
+
+        return tester_results, label, pred, category, text_units
+
+    def analyze(self, idx: int):
+        return self[idx]
+
+    def get_labels_and_preds(self):
+        return self.labels, self.preds
+
+    def get_text_units(self):
+        return self.text_units
+
+    def analyze_all(self):
+
+        # retrieve all the results
+        for row_tester_results, row_label, row_pred, row_category, row_text_units in tqdm(self):
+            assert len(row_tester_results) == len(self.testers)
+            if row_tester_results[0] is not None:
+                self._save(row_tester_results, row_label, row_pred, row_category)
+                self.labels.append(row_label)
+                self.preds.append(row_pred)
+                self.text_units.append(row_text_units)
+
+        assert len(self.res_history) == len(self.testers)
+
+        # result post-processing
+        # now the results are stored and categorized in the 'res_history' variable
+        for tester_res in self.res_history:
+
+            assert isinstance(tester_res, dict)
+
+            for category in tester_res:
+                tester_res_by_cat = tester_res[category]
+
+                if tester_res_by_cat is not None:
+
+                    assert isinstance(tester_res_by_cat, (dict, TestResultCollector))
+
+                    if isinstance(tester_res_by_cat, dict):
+
+                        for key in tester_res_by_cat:
+                            assert isinstance(tester_res_by_cat[key], TestResultCollector)
+                            assert len(tester_res_by_cat[key]) > 0
+
+                            # normalization
+                            tester_res_by_cat[key].transform_all(lambda x: x / self.counts[category])
+
+                    elif isinstance(tester_res_by_cat, TestResultCollector):
+
+                        assert len(tester_res_by_cat) > 0
+
+                        # normalization
+                        tester_res_by_cat.transform_all(lambda x: x / self.counts[category])
+
+        return copy.deepcopy(self.res_history)
---
+++ attention/testers.py
@@ -0,0 +1,239 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from utils.result_collector import TestResultCollector
+
+
+class GenericAttributeAttentionTest(object):
+    """
+    This class analyzes the attention paid by some model on matching attributes by
+    examining its attention maps.
+    It produces the following results:
+    - lr_match_attr_attn_loc: mask that displays for each layer and head if the
+    attention paid by the model on the corresponding attributes of the left and
+    right entities is greater than the average attention between each pair of
+    attributes
+    - rl_match_attr_attn_loc: mask that displays for each layer and head if the
+    attention paid by the model on the corresponding attributes of the right and
+    left entities is greater than the average attention between each pair of
+    attributes
+    - match_attr_attn_loc: mask obtained by getting the maximum values between the
+    previous two masks
+    - match_attr_attn_over_mean: above-average attention paid by the model on each
+    pair of attributes of the two entities
+    """
+
+    def __init__(self, permute=False, model_attention_grid=(12, 12)):
+        assert isinstance(permute, bool), "Wrong data type for parameter 'permute'."
+        assert isinstance(model_attention_grid, tuple), "Wrong data type for parameter 'model_attention_grid'."
+        assert len(model_attention_grid) == 2, "'model_attention_grid' has to specify two dimensions."
+        assert model_attention_grid[0] > 0 and model_attention_grid[
+            1] > 0, "Wrong value for parameter 'model_attention_grid'."
+
+        self.permute = permute
+        self.model_attention_grid = model_attention_grid
+        self.result_names = ['lr_match_attr_attn_loc', 'rl_match_attr_attn_loc',
+                             'match_attr_attn_loc']
+
+        self.property_mask_res = ['match_attr_attn_over_mean', 'avg_attr_attn']
+        self.result_names += self.property_mask_res
+
+        mask = np.zeros(model_attention_grid)
+        attr_attn_3_last = mask.copy()
+        attr_attn_3_last[-3:, :] = 1
+        attr_attn_last_1 = mask.copy()
+        attr_attn_last_1[-1, :] = 1
+        attr_attn_last_2 = mask.copy()
+        attr_attn_last_2[-2, :] = 1
+        attr_attn_last_3 = mask.copy()
+        attr_attn_last_3[-3, :] = 1
+        self.cond_prop_mask_res = {'attr_attn_3_last': attr_attn_3_last,
+                                   'attr_attn_last_1': attr_attn_last_1,
+                                   'attr_attn_last_2': attr_attn_last_2,
+                                   'attr_attn_last_3': attr_attn_last_3,
+                                   'avg_attr_attn_3_last': attr_attn_3_last.copy(),
+                                   'avg_attr_attn_last_1': attr_attn_last_1.copy(),
+                                   'avg_attr_attn_last_2': attr_attn_last_2.copy(),
+                                   'avg_attr_attn_last_3': attr_attn_last_3.copy(), }
+        self.result_names += list(self.cond_prop_mask_res)
+
+    def _test_attr_attention(self, attn_map: np.ndarray):
+
+        assert isinstance(attn_map, np.ndarray), "Wrong data type for parameter 'attn_map'."
+
+        res = {}
+        n = attn_map.shape[0] // 2
+
+        # extract the attention between corresponding left-to-right and
+        # right-to-left attributes
+        lr_match_attr_attn = np.array([])
+        rl_match_attr_attn = np.array([])
+        for idx in range(n):
+            if self.permute:
+                lr = attn_map[idx, n - idx].item()
+                rl = attn_map[idx - n, idx].item()
+            else:
+                lr = attn_map[idx, n + idx].item()
+                rl = attn_map[idx + n, idx].item()
+            lr_match_attr_attn = np.append(lr_match_attr_attn, lr)
+            rl_match_attr_attn = np.append(rl_match_attr_attn, rl)
+
+        # check if these attention scores are greater than the average score
+        # if all these attention scores are over the mean then output 1 in the mask
+        m = attn_map.mean().item()
+        res['lr_match_attr_attn_loc'] = int((lr_match_attr_attn > m).sum() >= n - 1)
+        res['rl_match_attr_attn_loc'] = int((rl_match_attr_attn > m).sum() >= n - 1)
+
+        # diff_attr_attn_lr = np.array([])
+        # diff_attr_attn_rl = np.array([])
+        # for idx in range(n):
+        #   lr = np.concatenate(
+        #       (attn_map[idx, n : n + idx], attn_map[idx, n + idx + 1:])
+        #       ).mean().item()
+        #   rl = np.concatenate(
+        #       (attn_map[n : n + idx, idx], attn_map[n + idx + 1:, idx])
+        #       ).mean().item()
+        #   diff_attr_attn_lr = np.append(diff_attr_attn_lr, lr)
+        #   diff_attr_attn_rl = np.append(diff_attr_attn_rl, rl)
+        # res['diff_lr'] = diff_attr_attn_lr
+        # res['diff_rl'] = diff_attr_attn_rl
+
+        # save the mask that indicates which pair of attributes generates an
+        # attention greater than the average score
+        res['match_attr_attn_over_mean'] = attn_map > m
+        res['attr_attn_3_last'] = attn_map > m
+        res['attr_attn_last_1'] = attn_map > m
+        res['attr_attn_last_2'] = attn_map > m
+        res['attr_attn_last_3'] = attn_map > m
+        res['avg_attr_attn'] = attn_map.copy()
+        res['avg_attr_attn_3_last'] = attn_map.copy()
+        res['avg_attr_attn_last_1'] = attn_map.copy()
+        res['avg_attr_attn_last_2'] = attn_map.copy()
+        res['avg_attr_attn_last_3'] = attn_map.copy()
+
+        return res
+
+    def test(self, left_entity: pd.Series, right_entity: pd.Series,
+             attn_params: dict):
+
+        assert isinstance(left_entity, pd.Series), "Wrong data type for parameter 'left_entity'."
+        assert isinstance(right_entity, pd.Series), "Wrong data type for parameter 'right_entity'."
+        assert isinstance(attn_params, dict), "Wrong data type for parameter 'attn_params'."
+        assert 'attns' in attn_params, "Attention maps parameter non found."
+
+        attr_attns = attn_params['attns']
+
+        if attr_attns is None:
+            return None
+
+        n_layers = attr_attns.shape[0]
+        n_heads = attr_attns.shape[1]
+        n_attrs = attr_attns[0][0].shape[0]
+        assert self.model_attention_grid == (n_layers, n_heads)
+
+        # initialize the result collector
+        res_collector = TestResultCollector()
+        for result_name in self.result_names:
+            if result_name in self.property_mask_res or result_name in self.cond_prop_mask_res:
+                res_collector.save_result(np.zeros((n_attrs, n_attrs)), result_name)
+            else:
+                res_collector.save_result(np.zeros((n_layers, n_heads)), result_name)
+
+        # loop over the attention maps and analize them
+        for layer in range(n_layers):
+            for head in range(n_heads):
+                attn_map = attr_attns[layer][head]
+
+                # analyze the current attention map
+                test_res = self._test_attr_attention(attn_map)
+
+                # save the results in the collector
+                for result_name in test_res:
+                    if result_name in self.property_mask_res:
+                        res_collector.transform_result(result_name,
+                                                       lambda x: x + test_res[result_name])
+                    elif result_name in self.cond_prop_mask_res:
+                        if self.cond_prop_mask_res[result_name][layer][head]:
+                            res_collector.transform_result(result_name,
+                                                           lambda x: x + test_res[result_name])
+                    elif result_name in self.result_names:
+                        res_collector.update_result_value(layer, head,
+                                                          test_res[result_name],
+                                                          result_name)
+                    else:
+                        ValueError("No result name found.")
+
+        # update/add some results
+        res_collector.combine_results('lr_match_attr_attn_loc',
+                                      'rl_match_attr_attn_loc',
+                                      lambda x, y: np.maximum(x, y),              # FIXME: sum or average or maximum?
+                                      'match_attr_attn_loc')
+
+        for result_name in self.property_mask_res:
+            res_collector.transform_result(result_name,
+                                           lambda x: x / (n_layers * n_heads))
+        for result_name in self.cond_prop_mask_res:
+            res_collector.transform_result(result_name,
+                                           lambda x: x / (self.cond_prop_mask_res[result_name].sum()))
+
+        return res_collector
+
+    def _check_result_params(self, result: dict):
+        assert isinstance(result, dict), "Wrong data type for parameter 'result'."
+
+        params = self.result_names
+        assert np.sum([param in result for param in params]) == len(params)
+
+    def plot(self, res_collector: TestResultCollector, plot_params: list = None,
+             out_dir=None, out_file_name_prefix=None, ax=None, labels=None,
+             vmin=0, vmax=1):
+
+        assert isinstance(res_collector, TestResultCollector), "Wrong data type for parameter 'res_collector'."
+        result = res_collector.get_results()
+        self._check_result_params(result)
+        if plot_params is not None:
+            assert isinstance(plot_params, list)
+            assert len(plot_params) > 0
+            for plot_param in plot_params:
+                assert plot_param in result
+
+        for param, score in result.items():
+
+            if plot_params is not None:
+                if param not in plot_params:
+                    continue
+
+            if ax is None:
+                fig, new_ax = plt.subplots(figsize=(10, 5))
+                fig.suptitle(param)
+            else:
+                new_ax = ax
+
+            assert new_ax is not None
+
+            _ = sns.heatmap(score, annot=True, fmt='.1f', ax=new_ax, vmin=vmin, vmax=vmax)
+            ylabel = 'layers'
+            xlabel = 'heads'
+            if param in self.property_mask_res:
+                xlabel, ylabel = 'attributes', 'attributes'
+
+            new_ax.set_xlabel(xlabel)
+
+            if labels:
+                new_ax.set_ylabel(ylabel)
+            else:
+                new_ax.set_yticks([])
+
+            if out_dir is not None:
+                if out_file_name_prefix is not None:
+                    out_plot_file_name = '{}_{}.pdf'.format(out_file_name_prefix, param)
+                else:
+                    out_plot_file_name = '{}.pdf'.format(param)
+
+                if ax is None:
+                    plt.savefig(os.path.join(out_dir, out_plot_file_name), bbox_inches='tight')
+
+            if ax is None:
+                plt.show()
