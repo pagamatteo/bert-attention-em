89318b857fb75e80f10e094b56e3f9350a4d5a4d
pagamatteo <pagamatteo@gmail.com>
2021-08-05 10:08:18 +0200

    Add more gradient tests and plots.

--- explanation/gradient/plot_gradients.py
+++ explanation/gradient/plot_gradients.py
@@ -3,9 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from explanation.gradient.extractors import EntityGradientExtractor
+import os
+import pickle
 
 
-def plot_grads(grad_data: dict, target_entity: str, title: str = None):
+def plot_grads(grad_data: dict, target_entity: str, title: str = None, out_plot_name: str = None,
+               ignore_special: bool = False, max_y = None):
     assert isinstance(grad_data, dict), "Wrong data type for parameter 'grad_data'."
     params = ['all', 'all_grad', 'left', 'left_grad', 'right', 'right_grad']
     assert all([p in grad_data for p in params]), "Wrong data format for parameter 'grad_data'."
@@ -18,6 +21,11 @@ def plot_grads(grad_data: dict, target_entity: str, title: str = None):
     plt.subplots(figsize=(20, 10))
 
     x = grad_data[target_entity]
+    sep_idxs = list(np.where(np.array(x) == '[SEP]')[0])
+    skip_idxs = [0] + sep_idxs
+    if ignore_special:
+        if x[0] == '[CLS]':
+            x = [x[i] for i in range(len(x)) if i not in skip_idxs]
 
     # check for duplicated labels
     label_counts = pd.Series(x).value_counts()
@@ -30,20 +38,72 @@ def plot_grads(grad_data: dict, target_entity: str, title: str = None):
                 new_x[nul_idx] = f'{nul}_{i}'  # concatenating the duplicated label with an incremental id
         x = new_x.copy()
 
-    y = grad_data[f'{target_entity}_grad']
-    yerr = None
-    if f'{target_entity}_error_grad' in grad_data:
-        yerr = grad_data[f'{target_entity}_error_grad']
+    if isinstance(grad_data[f'{target_entity}_grad'], dict):
+        for m in grad_data[f'{target_entity}_grad']:
 
-    plt.bar(x, y, yerr=yerr)
+            if m not in ['avg']:
+                continue
+
+            y = grad_data[f'{target_entity}_grad'][m]
+            if ignore_special:
+                if grad_data[target_entity][0] == '[CLS]':
+                    y = [y[i] for i in range(len(y)) if i not in skip_idxs]
+
+            yerr = None
+            if f'{target_entity}_error_grad' in grad_data:
+                yerr = grad_data[f'{target_entity}_error_grad']
+            barlist = plt.bar(x, y, yerr=yerr, label=m)
+            max_grads_idxs = np.array(y).argsort()[-4:][::-1]
+            for max_grads_idx in max_grads_idxs:
+                barlist[max_grads_idx].set_color('r')
+    else:
+        y = grad_data[f'{target_entity}_grad']
+        if ignore_special:
+            if grad_data[target_entity][0] == '[CLS]':
+                y = [y[i] for i in range(len(y)) if i not in skip_idxs]
+
+        yerr = None
+        if f'{target_entity}_error_grad' in grad_data:
+            yerr = grad_data[f'{target_entity}_error_grad']
+        barlist = plt.bar(x, y, yerr=yerr)
+        max_grads_idxs = np.array(y).argsort()[-4:][::-1]
+        for max_grads_idx in max_grads_idxs:
+            barlist[max_grads_idx].set_color('r')
+
     plt.xticks(rotation=90)
+    if max_y:
+        plt.ylim(0, max_y)
     if title is not None:
         plt.title(title)
+    plt.legend()
+    plt.subplots_adjust(bottom=0.15)
+    fig = plt.gcf()
     plt.show()
+    decision = input("Press Enter to continue or press S to save the plot and continue...")
+    if decision == "S":
+        fig.savefig(out_plot_name, bbox_inches='tight')
 
 
-def plot_batch_grads(grads_data: list, target_entity: str):
+def plot_batch_grads(grads_data: list, target_entity: str, title_prefix: str = None, out_plot_name: str = None,
+                     ignore_special: bool = False):
     EntityGradientExtractor.check_extracted_grad(grads_data)
+
+    # get max gradient to have an upper bound for the y-axis when plotting
+    max_grad = 0
+    for g in grads_data:
+        text_units = g['grad'][f'{target_entity}']
+        sep_idxs = list(np.where(np.array(text_units) == '[SEP]')[0])
+        skip_idxs = [0] + sep_idxs
+        if isinstance(g['grad'][f'{target_entity}_grad'], dict):
+            x = g['grad'][f'{target_entity}_grad']['avg']
+        else:
+            x = g['grad'][f'{target_entity}_grad']
+        if ignore_special:
+            if text_units[0] == '[CLS]':
+                x = [x[i] for i in range(len(x)) if i not in skip_idxs]
+        max_g = np.max(x)
+        if max_g > max_grad:
+            max_grad = max_g
 
     for idx, grad_data in enumerate(grads_data):
 
@@ -55,6 +115,177 @@ def plot_batch_grads(grads_data: list, target_entity: str):
         label = grad_data['label']
         prob = grad_data['prob']
         pred = grad_data['pred']
-        title = f"Gradients for item#{idx} - label: {label} - pred: {pred} - prob: {prob}"
+        title = f"gradients for item#{idx} - label: {label} - pred: {pred} - prob: {prob}"
 
-        plot_grads(grad, target_entity, title=title)
+        if title_prefix is not None:
+            title = f'{title_prefix} {title}'
+
+        plot_grads(grad, target_entity, title=title, out_plot_name=f'{out_plot_name}_{idx}.pdf',
+                   ignore_special=ignore_special, max_y=max_grad)
+
+
+def plot_multi_use_case_grads(conf, sampler_conf, fine_tune, grads_conf, use_cases, out_dir, grad_agg_metrics=['avg'],
+                              plot_type='box', ignore_special: bool = True, out_plot_name: str = None):
+    assert isinstance(use_cases, list), "Wrong data type for parameter 'use_cases'."
+    assert len(use_cases) > 0, "Empty use case list."
+    grad_agg_available_metrics = ['sum', 'avg', 'median', 'max']
+    assert all([m in grad_agg_available_metrics for m in grad_agg_metrics]), "Wrong metric names."
+    plot_types = ['box', 'error']
+    assert plot_type in plot_types
+    if plot_type == 'box':
+        assert len(grad_agg_metrics) == 1, "Only one metric supported in the 'box' plot type."
+
+    tok = conf['tok']
+    size = sampler_conf['size']
+    grad_text_unit = grads_conf['text_unit']
+    grad_special_tokens = grads_conf['special_tokens']
+
+    ncols = 4
+    nrows = 3
+    if len(use_cases) == 1:
+        ncols = 1
+        nrows = 1
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 12), sharey=True)
+    if len(use_cases) > 1:
+        axes = axes.flat
+    # loop over the use cases
+    for idx, use_case in enumerate(use_cases):
+        out_fname = f"{use_case}_{tok}_{size}_{fine_tune}_{grad_text_unit}_{grad_special_tokens}_ALL"
+        data_path = os.path.join(out_dir, use_case, out_fname)
+        # load grads data
+        uc_grad = pickle.load(open(f"{data_path}.pkl", "rb"))
+        # check grads data format
+        EntityGradientExtractor.check_extracted_grad(uc_grad)
+        first_data = None
+        for item in uc_grad:
+            if item is not None:
+                first_data = item
+                break
+        if not first_data:
+            continue
+        text_unit_names = first_data['grad']['all']
+        sep_idxs = list(np.where(np.array(text_unit_names) == '[SEP]')[0])
+        skip_idxs = [0] + sep_idxs
+        assert all([item['grad']['all'] == text_unit_names for item in uc_grad if item is not None])
+
+        if len(use_cases) > 1:
+            ax = axes[idx]
+        else:
+            ax = axes
+
+        uc_plot_data = {}
+        for item in uc_grad:
+            if item is not None:
+                for m in item['grad']['all_grad']:
+                    if m in grad_agg_metrics:
+
+                        x = item['grad']['all_grad'][m]
+                        if ignore_special:
+                            if text_unit_names[0] == '[CLS]':
+                                x = [x[i] for i in range(len(x)) if i not in skip_idxs]
+
+                        if m not in uc_plot_data:
+                            uc_plot_data[m] = [x]
+                        else:
+                            uc_plot_data[m].append(x)
+
+        if ignore_special:
+            text_unit_names = [text_unit_names[i] for i in range(len(text_unit_names)) if i not in skip_idxs]
+
+        columns = []
+        num_columns = len(text_unit_names)
+        if grad_special_tokens and not ignore_special:
+            num_columns -= 3
+        if not ignore_special:
+            columns.append('[CLS]')
+        half_columns = num_columns // 2
+        for num in range(1, half_columns + 1):
+            columns.append(f'l_{num}')
+        if not ignore_special:
+            columns.append('[SEP]')
+        for num in range(1, half_columns + 1):
+            columns.append(f'r_{num}')
+        if not ignore_special:
+            columns.append('[SEP]')
+
+        for metric in uc_plot_data:
+
+            uc_plot_metric_table = pd.DataFrame(uc_plot_data[metric], columns=columns)
+
+            if plot_type == 'error':
+                uc_plot_metric_table_stats = uc_plot_metric_table.describe()
+                medians = uc_plot_metric_table_stats.loc['50%', :].values
+                percs_25 = uc_plot_metric_table_stats.loc['25%', :].values
+                percs_75 = uc_plot_metric_table_stats.loc['75%', :].values
+                uc_plot_metric_data = {
+                    'x': range(len(uc_plot_metric_table_stats.columns)),
+                    'y': medians,
+                    'yerr': [medians - percs_25, percs_75 - medians],
+                }
+
+                ax.errorbar(**uc_plot_metric_data, alpha=.75, fmt=':', capsize=3, capthick=1, label=metric)
+                uc_plot_metric_data_area = {
+                    'x': uc_plot_metric_data['x'],
+                    'y1': percs_25,
+                    'y2': percs_75
+                }
+                ax.fill_between(**uc_plot_metric_data_area, alpha=.25)
+                ax.set_xticks(range(len(uc_plot_metric_table.columns)))
+                ax.set_xticklabels(uc_plot_metric_table.columns)
+                ax.legend()
+
+            elif plot_type == 'box':
+                uc_plot_metric_table.boxplot(ax=ax)
+            else:
+                raise NotImplementedError("Wrong plot type.")
+
+        ax.set_title(use_case)
+        if idx % ncols == 0:
+            ax.set_ylabel('Gradient')
+        ax.set_xlabel('Attributes')
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.3)
+    if out_plot_name:
+        plt.savefig(out_plot_name, bbox_inches='tight')
+    plt.show()
+
+
+def plot_top_grad_stats(stats_data: dict, out_plot_name: str = None):
+    ncols = 4
+    nrows = 3
+    if len(stats_data) == 1:
+        ncols = 1
+        nrows = 1
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(20, 10), sharey=True)
+    if len(stats_data) > 1:
+        axes = axes.flat
+    for idx, use_case in enumerate(stats_data):
+
+        if len(stats_data) > 1:
+            ax = axes[idx]
+        else:
+            ax = axes
+
+        use_case_stats = stats_data[use_case]
+        use_case_stats.plot(kind='bar', stacked=True, ax=ax, legend=False, rot=0)
+        ax.set_title(use_case, fontsize=18)
+        if idx % ncols == 0:
+            ax.set_ylabel('Percent', fontsize=20)
+        ax.xaxis.set_tick_params(labelsize=18)
+        ax.yaxis.set_tick_params(labelsize=20)
+
+        # for p in ax.patches:
+        #     width, height = p.get_width(), p.get_height()
+        #     x, y = p.get_xy()
+        #     ax.text(x + width / 2,
+        #             y + height / 2,
+        #             '{:.0f} %'.format(height),
+        #             horizontalalignment='center',
+        #             verticalalignment='center')
+
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor=(0.73, 0.08), ncol=4, fontsize=20)
+    plt.subplots_adjust(wspace=0.1, hspace=0.4)
+    if out_plot_name:
+        plt.savefig(out_plot_name, bbox_inches='tight')
+    plt.show()
--- test/gradient_test.py
+++ test/gradient_test.py
@@ -1,34 +1,47 @@
 import os
+import time
+
+import pandas as pd
 from transformers import AutoTokenizer
 from utils.general import get_dataset, get_model, get_sample
 from pathlib import Path
-from explanation.gradient.plot_gradients import plot_grads, plot_batch_grads
+from explanation.gradient.plot_gradients import plot_grads, plot_batch_grads, plot_multi_use_case_grads,\
+    plot_top_grad_stats
 from explanation.gradient.extractors import EntityGradientExtractor, AggregateAttributeGradient
+from explanation.gradient.analyzers import TopKGradientAnalyzer
 import logging
 from multiprocessing import Process
-
+import pickle
 
 PROJECT_DIR = Path(__file__).parent.parent
 MODELS_DIR = os.path.join(PROJECT_DIR, 'results', 'models')
 RESULT_DIR = os.path.join(PROJECT_DIR, 'results', 'gradient_analysis')
 
 
-def run_gradient_test(conf, use_case, fine_tune, sampler_conf, text_unit, special_tokens, RESULT_DIR):
-    use_case_conf = conf.copy()
-    use_case_conf['use_case'] = use_case
-    dataset = get_dataset(use_case_conf)
-    tok = use_case_conf['tok']
-    model_name = use_case_conf['model_name']
+def run_gradient_test(conf, sampler_conf, fine_tune, grad_params, models_dir, res_dir):
+    assert isinstance(grad_params, dict), "Wrong data type for parameter 'grad_params'."
+    params = ['text_unit', 'special_tokens']  # , 'agg', 'agg_target_cat']
+    assert all([p in grad_params for p in params])
+
+    dataset = get_dataset(conf)
+    tok = conf['tok']
+    model_name = conf['model_name']
+    use_case = conf['use_case']
+
+    grad_text_unit = grad_params['text_unit']
+    grad_special_tokens = grad_params['special_tokens']
+    # grad_agg = grad_params['agg']
+    # grad_agg_target_cat = grad_params['agg_target_cat']
 
     if fine_tune is not None:
-        model_path = os.path.join(MODELS_DIR, fine_tune, f"{use_case}_{tok}_tuned")
+        model_path = os.path.join(models_dir, fine_tune, f"{use_case}_{tok}_tuned")
     else:
         model_path = None
     model = get_model(model_name, fine_tune, model_path)
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
 
     complete_sampler_conf = sampler_conf.copy()
-    complete_sampler_conf['permute'] = use_case_conf['permute']
+    complete_sampler_conf['permute'] = conf['permute']
     sample = get_sample(dataset, complete_sampler_conf)
 
     # [END] MODEL AND DATA LOADING
@@ -36,24 +49,32 @@ def run_gradient_test(conf, use_case, fine_tune, sampler_conf, text_unit, specia
     entity_grad_extr = EntityGradientExtractor(
         model,
         tokenizer,
-        text_unit,
-        special_tokens=special_tokens,
+        grad_text_unit,
+        special_tokens=grad_special_tokens,
+        show_progress=True
     )
-    out_fname = f"{use_case}_{use_case_conf['tok']}_{sampler_conf['size']}_{fine_tune}_{text_unit}_{special_tokens}"
-    out_dir = os.path.join(RESULT_DIR, use_case, out_fname)
-    grads_data = entity_grad_extr.extract(sample, sample.max_len, out_path=out_dir)
+    out_fname = f"{use_case}_{tok}_{sampler_conf['size']}_{fine_tune}_{grad_text_unit}_{grad_special_tokens}_ALL"
+    out_dir = os.path.join(res_dir, use_case, out_fname)
+    # save grads data
+    entity_grad_extr.extract(sample, sample.max_len, out_path=out_dir)
 
-    # if text_unit == 'attrs' and agg:
-    #     aggregator = AggregateAttributeGradient(grads_data, target_categories=target_categories)
-    #     agg_grads = aggregator.aggregate(agg)
-    #     for cat in agg_grads:
-    #         if agg_grads[cat] is None:
-    #             logging.info(f"No {cat} data.")
-    #         else:
-    #             plot_grads(agg_grads[cat], 'all', title=f'{agg} {cat} gradients')
-    #
-    # else:
-    #     plot_batch_grads(grads_data, 'all')
+    # if grad_agg:        # aggregate gradient data
+    #     if grad_text_unit == 'attrs':
+    #         aggregator = AggregateAttributeGradient(grads_data, target_categories=grad_agg_target_cat)
+    #         agg_grads = aggregator.aggregate(grad_agg)
+    #     else:
+    #         raise NotImplementedError("No aggregation for non-attrs gradients.")
+
+
+def load_saved_grads_data(use_case, conf, sampler_conf, fine_tune, grad_conf):
+    tok = conf['tok']
+    size = sampler_conf['size']
+    text_unit = grad_conf['text_unit']
+    special_tokens = grad_conf['special_tokens']
+    out_fname = f"{use_case}_{tok}_{size}_{fine_tune}_{text_unit}_{special_tokens}_ALL"
+    data_path = os.path.join(RESULT_DIR, use_case, out_fname)
+    uc_grad = pickle.load(open(f"{data_path}.pkl", "rb"))
+    return uc_grad
 
 
 if __name__ == '__main__':
@@ -64,7 +85,7 @@ if __name__ == '__main__':
                  "Structured_Amazon-Google", "Structured_Walmart-Amazon", "Structured_Beer",
                  "Structured_iTunes-Amazon", "Textual_Abt-Buy", "Dirty_iTunes-Amazon", "Dirty_DBLP-ACM",
                  "Dirty_DBLP-GoogleScholar", "Dirty_Walmart-Amazon"]
-    # use_cases = ["Structured_Fodors-Zagats", "Structured_Beer"]
+    # use_cases = ["Structured_Fodors-Zagats"]
 
     conf = {
         'data_type': 'train',  # 'train', 'test', 'valid'
@@ -79,28 +100,73 @@ if __name__ == '__main__':
     }
 
     sampler_conf = {
-        'size': 2,
+        'size': 50,
         'target_class': 'both',  # 'both', 0, 1
         'seeds': [42, 42],  # [42 -> class 0, 42 -> class 1]
     }
 
     fine_tune = 'simple'  # None, 'simple', 'advanced'
 
-    text_unit = 'words'
-    special_tokens = False
-    agg = 'mean'
-    target_categories = ['all', 'all_pos', 'all_neg', 'all_pred_pos', 'all_pred_neg', 'tp', 'tn', 'fp', 'fn']
+    grad_conf = {
+        'text_unit': 'attrs',
+        'special_tokens': True,
+        'agg': None,  # 'mean'
+        'agg_target_cat': ['all', 'all_pos', 'all_neg', 'all_pred_pos', 'all_pred_neg', 'tp', 'tn', 'fp', 'fn']
+    }
+
+    experiment = 'plot_grad'  # 'compute_grad', 'plot_grad', 'word_grad_analysis', 'cmp_grad'
 
     # [END] PARAMS
 
-    # [BEGIN] MODEL AND DATA LOADING
+    start = time.time()
 
-    processes = [Process(target=run_gradient_test,
-                         args=(conf, use_case, fine_tune, sampler_conf, text_unit, special_tokens, RESULT_DIR,)) for
-                 use_case in use_cases]
+    if experiment == 'compute_grad':
+        processes = []
+        for use_case in use_cases:
+            uc_conf = conf.copy()
+            uc_conf['use_case'] = use_case
+            p = Process(target=run_gradient_test,
+                        args=(uc_conf, sampler_conf, fine_tune, grad_conf, MODELS_DIR, RESULT_DIR,))
+            processes.append(p)
 
-    for p in processes:
-        p.start()
+        for p in processes:
+            p.start()
 
-    for p in processes:
-        p.join()
+        for p in processes:
+            p.join()
+
+    elif experiment == 'plot_grad':
+        if grad_conf['text_unit'] == 'attrs':
+            grad_agg_metric = 'max'
+            out_plot_name = os.path.join(RESULT_DIR,
+                                         f"grad_{conf['tok']}_{sampler_conf['size']}_{grad_conf['text_unit']}_{grad_agg_metric}.pdf")
+            plot_multi_use_case_grads(conf, sampler_conf, fine_tune, grad_conf, use_cases, RESULT_DIR,
+                                      grad_agg_metrics=[grad_agg_metric], plot_type='box', out_plot_name=out_plot_name)
+            # plot_multi_use_case_grads(conf, sampler_conf, fine_tune, grad_conf, use_cases, RESULT_DIR,
+            #                           grad_agg_metrics=['max', 'avg'], plot_type='error')
+        else:
+            for use_case in use_cases:
+                uc_grad = load_saved_grads_data(use_case, conf, sampler_conf, fine_tune, grad_conf)
+                out_plot_name = os.path.join(RESULT_DIR, use_case,
+                                             f"grad_{grad_conf['text_unit']}_{grad_conf['special_tokens']}")
+                plot_batch_grads(uc_grad, 'all', title_prefix=use_case, out_plot_name=out_plot_name,
+                                 ignore_special=True)
+
+    elif experiment == 'word_grad_analysis':
+        stats_data = {}
+        for use_case in use_cases:
+            uc_grad = load_saved_grads_data(use_case, conf, sampler_conf, fine_tune, grad_conf)
+            analyzer = TopKGradientAnalyzer(uc_grad, topk=5)
+            analyzed_data = analyzer.analyze('pos', target_categories=['all', 'all_pos', 'all_neg'])
+            stats_data[use_case] = analyzed_data
+
+        out_plot_name = os.path.join(RESULT_DIR, f"word_grad_analysis_{conf['tok']}_{sampler_conf['size']}.pdf")
+        plot_top_grad_stats(stats_data, out_plot_name=out_plot_name)
+
+    elif experiment == 'cmp_grad':
+        pass
+    else:
+        raise ValueError("Experiment not found.")
+
+    end = time.time()
+    print(end - start)
